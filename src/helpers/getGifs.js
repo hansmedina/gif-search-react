0eba760f6c102dbcc4be0b3915d514c791596317
Hans Medina <hansmedina0@gmail.com>
2023-10-07 18:11:41 -0500

    Valorant se quita

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=lbnsDdBedmz2BhbY6re69CC3bjeefkea&q=Valorant=${ category }&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=lbnsDdBedmz2BhbY6re69CC3bjeefkea&q=${ category }&limit=10`
     const resp = await fetch( url );
 
     const { data } = await resp.json();
